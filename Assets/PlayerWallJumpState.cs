a9543ae05ddf337cb5dd1d5ac2a41db3b5cb9a51
FaaatPanda <markvalko1@gmail.com>
2024-06-07 00:02:45 +0300

    Git Init
    Wall Jump State

---
+++ Assets/PlayerWallJumpState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallJumpState : PlayerState
+{
+    public PlayerWallJumpState(Player _player, PlayerStateMachine stateMachine, string _animBoolName) : base(_player, stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        player.SetVelocity(5 * -player.facingDir, player.jumpForce);
+
+        stateTimer = .4f;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (stateTimer < 0)
+            stateMachine.ChangeState(player.airState);
+
+        if(player.IsGroundDetected())
+            stateMachine.ChangeState(player.idleState);
+        
+    }
+}
